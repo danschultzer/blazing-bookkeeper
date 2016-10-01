654a4fd89a6bfba0a38900e135e20891ac1b3842
Dan Schultzer <dan@dreamconception.com>
2016-09-30 17:48:45 -0700

    Make releases work
    - Moved thirdparty dependencies to Resources location
    - Removed most tesseract trained data, just keeping eng and osd (orientation)
    - Update readme with PKG_CONFIG_PATH instructions

---
+++ src/utils/thirdparty_env.js
@@ -0,0 +1,26 @@
+import getResourcesPath from './resources'
+
+var thirdpartyPath = getResourcesPath('thirdparty'),
+  env = {};
+
+if (process.platform == 'darwin') {
+  // Set 3rd party binaries and libraries
+  env.PATH = [
+    "$PATH",
+    thirdpartyPath + "/poppler/bin",
+    thirdpartyPath + "/tesseract/bin"
+  ].join(':');
+
+  env.DYLD_LIBRARY_PATH = [
+    "$DYLD_LIBRARY_PATH",
+    "/System/Library/Frameworks/ImageIO.framework/Versions/A/Resources/", // Core Graphics package
+    thirdpartyPath + "/dependencies/lib",
+    thirdpartyPath + "/poppler/lib",
+    thirdpartyPath + "/opencv/lib",
+    thirdpartyPath + "/tesseract/lib"
+  ].join(':');
+
+  env.TESSDATA_PREFIX = thirdpartyPath + "/tesseract/share/tessdata";
+}
+
+export default env;
