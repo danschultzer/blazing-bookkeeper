d0eb9c6b7b128f8e3f7a9c196d6aad4f3ec8b899
Benjamin Schultzer <benjamin@schultzer.com>
2016-11-09 22:59:06 -0800

    Fix selectall

--- src/menu/edit_menu_template.js
+++ src/menu/edit_menu_template.js
@@ -22,11 +22,7 @@ export default {
       role: 'paste'
     },
     {
-      label: 'Select All',
-      accelerator: 'CmdOrCtrl+A',
-      click: function() {
-          BrowserWindow.getFocusedWindow().webContents.executeJavaScript("document.dispatchEvent(new CustomEvent('selectAll'));");
-      }
+      role: 'selectall'
     }
   ]
 };
