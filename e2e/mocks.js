c67dd5e9587fc281ef5217e68a644c3767f04d07
Benjamin Schultzer <benjamin@schultzer.com>
2016-12-06 16:53:17 -0800

    Removed curly braces

--- e2e/mocks.js
+++ e2e/mocks.js
@@ -1,5 +1,3 @@
 const path = require('path')
 const electron = require('electron')
-electron.dialog.showOpenDialog = (opts, cb) => {
-  cb([path.join(__dirname, 'support', 'readable.pdf')])
-}
+electron.dialog.showOpenDialog = (opts, cb) => cb([path.join(__dirname, 'support', 'readable.pdf')])
