c8bcc3a7f54ddc5a21c80b262c9a5f71423d1aec
Benjamin Schultzer <benjamin@schultzer.com>
2016-12-06 18:23:47 -0800

    Fixed indention

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -5,10 +5,10 @@ var jetpack = require('fs-jetpack')
 var rollup = require('rollup').rollup
 
 var nodeBuiltInModules = ['assert', 'buffer', 'child_process', 'cluster',
-    'console', 'constants', 'crypto', 'dgram', 'dns', 'domain', 'events',
-    'fs', 'http', 'https', 'module', 'net', 'os', 'path', 'process', 'punycode',
-    'querystring', 'readline', 'repl', 'stream', 'string_decoder', 'timers',
-    'tls', 'tty', 'url', 'util', 'v8', 'vm', 'zlib']
+  'console', 'constants', 'crypto', 'dgram', 'dns', 'domain', 'events',
+  'fs', 'http', 'https', 'module', 'net', 'os', 'path', 'process', 'punycode',
+  'querystring', 'readline', 'repl', 'stream', 'string_decoder', 'timers',
+  'tls', 'tty', 'url', 'util', 'v8', 'vm', 'zlib']
 
 var electronBuiltInModules = ['electron']
 
