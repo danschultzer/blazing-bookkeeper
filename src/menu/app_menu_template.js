028bb414d08923b394be5ac12775e6afa2759814
Benjamin Schultzer <benjamin@schultzer.com>
2016-12-06 18:12:36 -0800

    Refactored app_menu
    Export moved to the bottom.

--- src/menu/app_menu_template.js
+++ src/menu/app_menu_template.js
@@ -1,35 +1,27 @@
 import { app } from 'electron'
 
-export default {
+const appMenuTemplate = {
   label: app.getName(),
-  submenu: [
-    {
-      role: 'about'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      role: 'services',
-      submenu: []
-    },
-    {
-      type: 'separator'
-    },
-    {
-      role: 'hide'
-    },
-    {
-      role: 'hideothers'
-    },
-    {
-      role: 'unhide'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      role: 'quit'
-    }
-  ]
+  submenu: [{
+    role: 'about'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'services',
+    submenu: []
+  }, {
+    type: 'separator'
+  }, {
+    role: 'hide'
+  }, {
+    role: 'hideothers'
+  }, {
+    role: 'unhide'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'quit'
+  }]
 }
+
+export default appMenuTemplate
